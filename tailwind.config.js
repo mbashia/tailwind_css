c1cc84b74681d554f0c47f65e1a02c4b8b44bdbb
mbashia <mbashiavictor@gmail.com>
2023-06-26 08:15:52 +0300

    more learning

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                primary: '##F6363',
+                secondary: {
+                    100: '#E2E2d5',
+                    200: '#888883',
+                }
+
+            },
+            fontFamily: {
+                boduy: ['nunito'],
+            },
+
+        },
+    },
+    variants: {},
+    plugins: [],
+}
